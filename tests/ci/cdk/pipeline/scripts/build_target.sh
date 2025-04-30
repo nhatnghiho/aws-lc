3e7e6981b70b95c91b3e9bca82ff5ec1c484182c
Nghi Ho <hhaileyh@amazon.com>
2025-04-30 09:15:58 -0700

    Remove unnecessary vpc definitions

--- tests/ci/cdk/pipeline/scripts/build_target.sh
+++ tests/ci/cdk/pipeline/scripts/build_target.sh
@@ -160,12 +160,12 @@ if [[ ${BUILD_TYPE} == "docker" ]]; then
     exit 1
   fi
 
-  if [[ ${PLATFORM} == "linux" ]]; then
-    build_linux_docker_images
-  elif [[ ${PLATFORM} == "windows" ]]; then
-    build_win_docker_images
-  fi
-  exit 0
+#  if [[ ${PLATFORM} == "linux" ]]; then
+#    build_linux_docker_images
+#  elif [[ ${PLATFORM} == "windows" ]]; then
+#    build_win_docker_images
+#  fi
+#  exit 0
 fi
 
 if [[ ${BUILD_TYPE} == "ci" ]]; then
